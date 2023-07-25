fdf3b21792feceb7fd97ed94d28fe6b323699b6f
jer-myah <aluefuaehiabhi@gmail.com>
2023-07-25 11:50:14 +0100

    Change users id to ulid

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use HasUlids;
 
     /**
      * The attributes that are mass assignable.
